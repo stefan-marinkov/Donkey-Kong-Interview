da4a70591588d7bb9365a3ccdef58de3dc10714a
BIT Student <classroom@bgit.rs>
2021-04-07 14:37:19 +0200

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.scss';
 
 import FrontEndPage from './page/FrontEndPage'
@@ -6,12 +6,30 @@ import BackEndPage from './page/BackEndPage'
 
 import { Link, Route, Switch } from 'react-router-dom'
 
+
+
 function App() {
+
+  const report = '/report'
+  const companies = '/companies'
+  const candidates = '/candidates'
+  const users = '/users'
+  const baseUrl = 'http://localhost:3333/api'
+
+  useEffect(() => {
+    fetch(baseUrl + companies)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+
+
   return (
     <div className="App">
+      <FrontEndPage />
+      <BackEndPage />
       <Switch>
-      <Route path='/home' ><FrontEndPage /></Route>
-      <Route path='/home' ><BackEndPage /></Route>
+        <Route path='/home' ><FrontEndPage /></Route>
+        <Route path='/home' ><BackEndPage /></Route>
       </Switch>
     </div>
   );
